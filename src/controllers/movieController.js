9225919bbcd969095501ebad9898044b99cf1192
Luis Gamboa <luis_gamboa17@hotmail.com>
2026-04-08 14:22:56 -0600

    console logs para verificar ip

--- src/controllers/movieController.js
+++ src/controllers/movieController.js
@@ -17,8 +17,13 @@ export class MovieController {
             return res.status(404).json({ message: 'No movies found' });
         }
 
-        console.log('Ip: ', req.ip, ' - User Agent: ', req.get('User-Agent'), ' - Endpoint: ', req.originalUrl, ' - Method: ', req.method)
-        console.log('Ip: ', req.headers["x-forwarded-for"] || req.socket.remoteAddress)
+        console.log('--- Headers Recibidos ---');
+        console.log('X-Forwarded-For:', req.headers['x-forwarded-for']);
+        console.log('X-Real-IP:', req.headers['x-real-ip']);
+        console.log('Remote Address:', req.socket.remoteAddress);
+        console.log('IP detectada por Express:', req.ip);
+
+        
         return res.status(200).json({
             status: 'success',
             totalItems: totalCount,
